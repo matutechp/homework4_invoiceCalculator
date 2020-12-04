b2aee54b8da7174201c3537737211714da6c8b73
matut <matut@SURFACE-MATTHEW>
2020-12-04 14:17:36 -0500

    Add project files.

---
+++ homework4_invoiceCalculator/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace homework4_invoiceCalculator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblDiscPer = new System.Windows.Forms.Label();
+            this.lblDiscAmo = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtDiscAmo = new System.Windows.Forms.TextBox();
+            this.txtDiscPer = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(24, 58);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(251, 29);
+            this.lblPrice.TabIndex = 0;
+            this.lblPrice.Text = "PRICE (SUBTOTAL)";
+            // 
+            // lblDiscPer
+            // 
+            this.lblDiscPer.AutoSize = true;
+            this.lblDiscPer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblDiscPer.Location = new System.Drawing.Point(24, 115);
+            this.lblDiscPer.Name = "lblDiscPer";
+            this.lblDiscPer.Size = new System.Drawing.Size(277, 29);
+            this.lblDiscPer.TabIndex = 1;
+            this.lblDiscPer.Text = "DISCOUNT PERCENT";
+            // 
+            // lblDiscAmo
+            // 
+            this.lblDiscAmo.AutoSize = true;
+            this.lblDiscAmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblDiscAmo.Location = new System.Drawing.Point(24, 178);
+            this.lblDiscAmo.Name = "lblDiscAmo";
+            this.lblDiscAmo.Size = new System.Drawing.Size(265, 29);
+            this.lblDiscAmo.TabIndex = 2;
+            this.lblDiscAmo.Text = "DISCOUNT AMOUNT";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTotal.Location = new System.Drawing.Point(24, 228);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(97, 29);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "TOTAL";
+            this.lblTotal.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.txtTotal.Location = new System.Drawing.Point(299, 228);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(204, 35);
+            this.txtTotal.TabIndex = 4;
+            // 
+            // txtDiscAmo
+            // 
+            this.txtDiscAmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.txtDiscAmo.Location = new System.Drawing.Point(299, 176);
+            this.txtDiscAmo.Name = "txtDiscAmo";
+            this.txtDiscAmo.ReadOnly = true;
+            this.txtDiscAmo.Size = new System.Drawing.Size(204, 35);
+            this.txtDiscAmo.TabIndex = 5;
+            // 
+            // txtDiscPer
+            // 
+            this.txtDiscPer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.txtDiscPer.Location = new System.Drawing.Point(299, 115);
+            this.txtDiscPer.Name = "txtDiscPer";
+            this.txtDiscPer.ReadOnly = true;
+            this.txtDiscPer.Size = new System.Drawing.Size(204, 35);
+            this.txtDiscPer.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.txtPrice.Location = new System.Drawing.Point(299, 58);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(204, 35);
+            this.txtPrice.TabIndex = 7;
+            this.txtPrice.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnExit.Location = new System.Drawing.Point(542, 205);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(202, 47);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnClear.Location = new System.Drawing.Point(540, 135);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(202, 47);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCalculate.Location = new System.Drawing.Point(542, 67);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(202, 47);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::homework4_invoiceCalculator.Properties.Resources.Project;
+            this.picLogo.Location = new System.Drawing.Point(750, 8);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(412, 404);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 11;
+            this.picLogo.TabStop = false;
+            this.picLogo.Click += new System.EventHandler(this.picLogo_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 424);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtDiscPer);
+            this.Controls.Add(this.txtDiscAmo);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDiscAmo);
+            this.Controls.Add(this.lblDiscPer);
+            this.Controls.Add(this.lblPrice);
+            this.Name = "frmMain";
+            this.Text = "INVOICE TOTAL";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblDiscPer;
+        private System.Windows.Forms.Label lblDiscAmo;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtDiscAmo;
+        private System.Windows.Forms.TextBox txtDiscPer;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.PictureBox picLogo;
+    }
+}
+
---
+++ homework4_invoiceCalculator/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework4_invoiceCalculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picLogo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPrice_TextChanged(object sender, EventArgs e)
+        {
+            txtDiscAmo.Clear();
+            txtDiscPer.Clear();
+            txtTotal.Clear();
+            float num1;
+            if (txtPrice.Text != "")
+            {
+                if (!float.TryParse(txtPrice.Text, out num1))
+                { 
+                    MessageBox.Show("Please enter a valid imput");
+                    txtPrice.Clear();
+                    return;
+                }
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPrice.Clear();
+            txtDiscAmo.Clear();
+            txtDiscPer.Clear();
+            txtTotal.Clear();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            
+            float price, percent, discAmo, total;
+            string sPercent, sDiscAmo, sTotal;
+            price = float.Parse(txtPrice.Text);
+            if (price < 0)
+            {
+                MessageBox.Show("Please enter a valid input");
+            }
+            else {
+                if (price == 0)
+                {
+                    percent = 0;
+                }
+                else if (price <= 99)
+                {
+                    percent = 1;
+
+                }
+                else if (price <= 199)
+                {
+                    percent = 2;
+                }
+                else if (price <= 299)
+                {
+                    percent = 3;
+                }
+                else
+                {
+                    percent = 4;
+                }
+                sPercent = percent.ToString();
+                percent = percent / 100;
+                discAmo = price * percent;
+                total = price - discAmo;
+                sTotal = total.ToString();
+                sDiscAmo = discAmo.ToString();
+                txtTotal.Text = "$" + sTotal;
+                txtDiscPer.Text = sPercent + ".00%";
+                txtDiscAmo.Text = "$" + sDiscAmo;
+            }
+        }
+    }
+}
